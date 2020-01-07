90c1edb7d9a9082474beceee136e22a26c7335ff
mirovois <mirovois@gmail.com>
2020-01-06 22:34:46 -0500

    commit3

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 var bill = {
+  values:[124,48,268,180,42],
+  tip:[],
+  amount:[],
+  calculateTip: function() {
+
+    for (var i=0;i<this.values.length;i++) {
+       if (this.values[i]<50) {
+       this.tip.push(this.values[i]*.1);
+      }
+      else if (this.values[i]>=50 && this.values[i]<200) {
+        this.tip.push(this.values[i]*.15);
+      }
+      else {
+          this.tip.push(this.values[i]*.2);
+     }
+  }
+},
+calculateAmount: function() {
+  for (var i=0;i<this.values.length;i++) {
+    this.amount.push(this.tip[i]+this.values[i]);
+  }
+}
+}
+bill.calculateTip();
+bill.calculateAmount();
+console.log(bill);
