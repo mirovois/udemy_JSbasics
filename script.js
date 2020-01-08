ece7333f4efc04f2f29bd38e94c9005479ee1ec4
mirovois <mirovois@gmail.com>
2020-01-08 12:42:09 -0500

    commit4: 1.Added the second object for MArk's family. 2.Created a function for calculating the average. 3. Created a function for calculating the one who had payed the highest average on tip.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var bill = {
+// --- Joh's object---
+var billJohn = {
   values:[124,48,268,180,42],
   tip:[],
   amount:[],
@@ -22,6 +23,57 @@ calculateAmount: function() {
   }
 }
 }
-bill.calculateTip();
-bill.calculateAmount();
-console.log(bill);
+billJohn.calculateTip();
+billJohn.calculateAmount();
+console.log(billJohn);
+
+// ---Mark's object---
+
+var billMark = {
+  values:[77,375,110,45],
+  tip:[],
+  amount:[],
+  calculateTip: function() {
+
+    for (var i=0;i<this.values.length;i++) {
+       if (this.values[i]<100) {
+       this.tip.push(this.values[i]*.1);
+      }
+      else if (this.values[i]>=100 && this.values[i]<300) {
+        this.tip.push(this.values[i]*.15);
+      }
+      else {
+          this.tip.push(this.values[i]*.2);
+     }
+  }
+},
+calculateAmount: function() {
+  for (var i=0;i<this.values.length;i++) {
+    this.amount.push(this.tip[i]+this.values[i]);
+  }
+}
+}
+billMark.calculateTip();
+billMark.calculateAmount();
+console.log(billMark);
+
+function calculateAverage(arr) {
+  var sum = 0;
+  for (var i =0; i<arr.length;i++) {
+    sum=sum+arr[i];
+  }
+  return sum/arr.length;
+}
+// console.log(calculateAverage(billJohn.values));
+
+function whoPayedMore() {
+  if(calculateAverage(billJohn.values)>calculateAverage(billMark.values)) {
+    console.log('John payed more in tip which on averge is '+
+      calculateAverage(billJohn.values));
+  }
+  else {console.log('Mark payed more in tip which on averge is '+
+    calculateAverage(billMark.values));
+
+  }
+  }
+  whoPayedMore();
